1760087bb07bbc1ab621209e9c477eed93db2fd6
1941012650Somesh <68364973+1941012650Somesh@users.noreply.github.com>
2021-01-17 19:04:51 +0530

    Update remove.java

--- remove.java
+++ remove.java
@@ -1,43 +1,17 @@
-import java.util.*;
-public class remove  
-{ 
-    static final int NO_OF_CHARS = 256;
-    static int[] getCharCountArray(String str) 
-    { 
-       int count[] = new int[NO_OF_CHARS]; 
-       for (int i = 0; i<str.length();  i++) 
-          count[str.charAt(i)]++; 
-         
-       return count; 
-    } 
-    static String removeDirtyChars(String str, String mask_str) 
-    { 
-      int count[]  = getCharCountArray(mask_str); 
-      int ip_ind  = 0, res_ind = 0; 
-        
-      char arr[] = str.toCharArray(); 
-        
-      while (ip_ind != arr.length) 
-      { 
-        char temp = arr[ip_ind]; 
-        if(count[temp] == 0) 
-        { 
-            arr[res_ind] = arr[ip_ind]; 
-            res_ind ++; 
-        } 
-            ip_ind++; 
-          
-      }     
-  
-      str = new String(arr); 
-      return str.substring(0, res_ind); 
-        
+public class Roman {
+    public static void main(String[] args) {
+        System.out.println(intToRoman(8));
+        System.out.println(intToRoman(88));
+        System.out.println(intToRoman(888));
+ 
     }
-    public static void main(String[] args) 
-    { 
-        Scanner sc=new Scanner(System.in);
-        String str = sc.nextLine(); 
-        String mask_str = sc.nextLine(); 
-        System.out.println(removeDirtyChars(str, mask_str)); 
-    } 
+    public static String intToRoman(int num)
+    {
+        String[] Thousands= {"","M","MM","MMM"};
+        String[] Hundreds={"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
+        String[] Tens={"","X","XX","XL","L","LX","LXX","LXXX","XC"};
+        String[] Units={"","I","II","III","IV","V","VI","VII","VIII","IX"};
+
+        return Thousands[num/1000]+Hundreds[(num%1000)/100]+Tens[(num%100)/10]+Units[num%10];
+    }
 }
